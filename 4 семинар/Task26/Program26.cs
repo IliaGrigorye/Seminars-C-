ff38c040c9d8a552e58dac05046bb2897ba8315e
Ilia Grigoryev <shirokagaricu@mail.ru>
2022-04-23 13:09:09 +0800

    add changes

---
+++ 4 семинар/Task26/Program26.cs
@@ -0,0 +1,24 @@
+﻿// Программа которая принимает на вход число и выдает количество цифр в числе
+//456 -> 3
+
+Console.Write("Введите число: ");
+int n; // = Convert.ToInt32(Console.ReadLine());
+
+while(!int.TryParse(Console.ReadLine(), out n))
+{
+    Console.Write("Ошибка, введите число: ");
+}
+
+Console.Write($"Число {n} ");
+if (n < 0)
+{
+    n = -n;
+}
+int quan = 0; //количество чисел
+for (int i = 0; i < n * 10; i++)
+{
+    n = n / 10 ;
+    quan++;
+}
+
+Console.WriteLine($"состоит из {quan} чисел");
---
+++ 4 семинар/Task30/Program30.cs
@@ -0,0 +1,21 @@
+﻿// Программа которая выводит массив из 8 элементов, заполненый 0 и 1 в случайном порядке
+
+//int[] array = new int[8];
+//for (int i = 0; i <= array.Length - 1; i++)
+//{
+//    array[i] = new Random().Next(0, 2);
+//    Console.Write(array[i] + "; ");
+//}
+//Console.WriteLine();
+
+int[] array = new int[8];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++){
+    array[i] = rand.Next(0, 2);
+    if (i == 0)
+        Console.Write("[" + array[i] + "; ");
+    else if (i == array.Length - 1)
+        Console.Write(array[i] + "]");
+    else
+        Console.Write(array[i] + "; ");
+}
